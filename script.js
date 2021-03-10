6bb00090d618034620c472dce9a4076b92db42e5
renaudesign <renaud.sourmail@gmail.com>
2021-03-10 13:52:20 +0100

    opti

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 function rollOver(element){
     let imgTag = element.getAttribute("id")
     let sourceOrigin = element.getAttribute("src")
-    console.log(sourceOrigin);
     let sourceEdited = sourceOrigin.replace(".jpg","_2.jpg")
     document.getElementById(imgTag).src = sourceEdited
 }
